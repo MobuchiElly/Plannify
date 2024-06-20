5197bfc361ead4517b8271775ace05e1bb43000a
MobuchiElly <ugwueleazer@gmail.com>
2024-06-20 09:36:03 +0100

    Fixed issues accessing localstorage after deployment

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [lightTheme, setLightTheme] = useState(true);
   return (
     <nav className='h-36 w-screen bg-gray-700 bottom-0 text-white flex items-center'>
-      <div className='relative w-full h-full py-3 mt-3 lg:mt-0 mb-4 lg:mb-0'>
+      <div className='relative w-full h-full py-3 mt-3 lg:mt-0 mb-2 lg:mb-0'>
         <h1 className='text-5xl text-center p-2 font-bold'>Plannify</h1>
         <h2 className='text-center text-2xl font-mono p-2 font-[500]'>Never Fail to Plan<span className='hidden lg:inline-flex'>...</span> <span className='sm:text-center hidden lg:inline-flex lg:text-justify'>Never Plan to Fail</span></h2>
         <button className='absolute right-10 hidden lg:inline-flex top-10' onClick={() => setLightTheme(!lightTheme)}>
