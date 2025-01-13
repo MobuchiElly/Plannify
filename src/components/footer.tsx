00b56cc2da7697e6d0fbd0b78d290f16b41bafcd
MobuchiElly <ugwueleazer@gmail.com>
2025-01-14 00:18:33 +0100

    fix: improved ui

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='shadow w-screen h-40 bg-gray-800 text-white flex justify-center items-center'>
       <div className='h-full m-5 flex flex-col justify-center items-center'>
         <div className='text-2xl'>Built by Buchi_Dev</div>
-        <div>For Personal Use</div>
+        <div className='text-sm'>For Personal Use</div>
       </div>
     </footer>
   )
